5777f2371ad7abf582e0ea651c6d8f4fd618c4a6
Dani <danizt96@gmail.com>
2023-05-30 21:25:59 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node'];
+    var member = {
+        name: 'foo',
+        age: 20,
+        skills: skills
+    };
+    return member;
+}
